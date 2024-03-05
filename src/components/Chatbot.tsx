5a776eb18d28b628524d47b5f4bc5a945ca22ced
vamsee <vamsee.somasi@gmail.com>
2024-03-05 12:45:40 +0530

    Add query and response to chat section

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -1,22 +1,42 @@
 import { SyntheticEvent, useState } from "react";
 import styles from "./Chatbot.module.css";
 
+type ChatData = {
+    query: undefined | string;
+    response: undefined | string;
+};
+
 // Chatbot launches a chat interface.
 const Chatbot = () => {
-    const [chats, setChats] = useState<string[]>([]);
+    const [chat, setChat] = useState<ChatData>({
+        query: undefined,
+        response: undefined,
+    });
 
     const defaultMessage =
         "Forget sifting through haystacks of information, query directly for targeted and insightful results.";
 
     const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
-        const formData = new FormData(event.target as HTMLFormElement);
-        const { query } = Object.fromEntries(formData.entries());
+        const form = event.target as HTMLFormElement;
+        const formData = new FormData(form);
+        const query = formData.get("query");
+
+        if (!query) return;
+
+        setChat({ query: query.toString(), response: undefined });
+
+        form.reset();
 
         // TODO: submit query and get response.
-        const response = "This text";
+        setTimeout(() => {
+            const response = "This text";
 
-        setChats(prev => [...prev, response]);
+            setChat(prev => {
+                prev.response = response;
+                return { ...prev };
+            });
+        }, 1000);
     };
 
     return (
@@ -25,14 +45,36 @@ const Chatbot = () => {
                 <span>Hexane</span>
             </div>
 
-            <div className={`${styles.chatSection} ${styles.center}`}>
-                <span>{defaultMessage}</span>
+            <div className={styles.chatSection}>
+                {chat.query ? (
+                    <DisplayResponse chat={chat} />
+                ) : (
+                    <div className={styles.defaultMessage}>
+                        <span>{defaultMessage}</span>
+                    </div>
+                )}
             </div>
 
             <form className={styles.input} onSubmit={handleSubmit}>
                 <input required name="query" />
                 <button type="submit">query</button>
             </form>
+        </div>
+    );
+};
+
+const DisplayResponse = ({ chat }: { chat: ChatData }) => {
+    const { query, response } = chat;
+
+    return (
+        <div className={styles.responseContainer}>
+            <details className={styles.query}>
+                <summary>Query</summary>
+                <span>{query}</span>
+            </details>
+            <div className={styles.responseMessage}>
+                <span>{response ?? "loading..."}</span>
+            </div>
         </div>
     );
 };
